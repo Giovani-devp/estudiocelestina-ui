87ea0616447c1403703f6490538b9fc53b4f35c5
TI-CJC <ti@jardimdacolina.com.br>
2024-03-18 15:08:50 -0300

    adc projeto/tailwind

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Tooltip, initTWE } from 'tw-elements';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  ngOnInit() {
+    initTWE({ Tooltip });
+  }
+}
