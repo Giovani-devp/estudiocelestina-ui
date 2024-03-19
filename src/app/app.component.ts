f84a01dbdeee1c8f35e20c45f3968de71b9e1523
TI-CJC <ti@jardimdacolina.com.br>
2024-03-19 15:46:36 -0300

    tipografia & cores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 import { Tooltip, initTWE } from 'tw-elements';
 
 @Component({
@@ -7,7 +8,20 @@ import { Tooltip, initTWE } from 'tw-elements';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  ngOnInit() {
-    initTWE({ Tooltip });
+   backgroundColor = 'bg-amarelo';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    interval(2000).subscribe(() => {
+      this.alternarCor();
+    });
+  }
+
+  alternarCor() {
+    const cores = ['bg-amarelo', 'bg-rosa', 'bg-laranja', 'bg-azul','bg-cinza', 'bg-azulsky',];
+    const corIndex = cores.indexOf(this.backgroundColor);
+    const proximaCorIndex = (corIndex + 1) % cores.length;
+    this.backgroundColor = cores[proximaCorIndex];
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./node_modules/tw-elements/js/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        amarelo: '#FFD300',
+        rosa: '#F984CD',
+        laranja: '#F8460D',
+        cinza: '#E6DED3',
+        verde:'#A59D47',
+        azulsky: '#A0DCE5',
+        azul: '#0079C7'
+      }
+    },
   },
   darkMode: "class",
   plugins: [require("tw-elements/plugin.cjs")]
