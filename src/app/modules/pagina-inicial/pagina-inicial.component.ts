5277c8a51e3f6952f3ddd970d70d20c5e557f315
TI-CJC <ti@jardimdacolina.com.br>
2024-03-20 16:45:05 -0300

    home atts

--- src/app/modules/pagina-inicial/pagina-inicial.component.ts
+++ src/app/modules/pagina-inicial/pagina-inicial.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class PaginaInicialComponent {
 
+  listFilmes:any[] = [
+    {label:'Lé com Cré', img: './assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01459.png'},
+    {label:'Cefalópodes', img: './assets/images/02_Cefalópodes/CEF_03_crop/CEF_AnimaticV3_17710.png'},
+    {label:'Câmbio, Desligo', img: './assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01459.png'},
+    {label:'Cefalópodes', img: './assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01459.png'},
+
+  ]
+
+  ngOnInit(): void {
+    this.playVideo();
+  }
+
+  playVideo(): void {
+    const video = document.getElementById('video') as HTMLVideoElement;
+    if (video) {
+      video.play().catch(error => {
+        console.error(error);
+      });
+    }
+  }
 }
