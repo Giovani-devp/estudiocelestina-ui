e1debc89b2bab4928c1db14ea2f9af9107d02d58
TI-CJC <ti@jardimdacolina.com.br>
2024-03-21 17:04:06 -0300

    att

--- src/app/modules/pagina-inicial/pagina-inicial.component.ts
+++ src/app/modules/pagina-inicial/pagina-inicial.component.ts
@@ -1,21 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-pagina-inicial',
   templateUrl: './pagina-inicial.component.html',
   styleUrls: ['./pagina-inicial.component.scss']
 })
-export class PaginaInicialComponent {
+export class PaginaInicialComponent implements OnInit, AfterViewInit{
 
   listFilmes:any[] = [
-    {label:'Lé com Cré', ano: '2018', img: './assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01459.png'},
-    {label:'Cefalópodes', ano: '2018', img: './assets/images/02_Cefalópodes/CEF_03_crop/CEF_AnimaticV3_17710.png'},
+    {label:'Lé com Cré', ano: '2018', img: './assets/images/01_Lé com Cré/LCC.png'},
+    {label:'Cefalópodes', ano: '2018', img: './assets/images/02_Cefalópodes/Cefalópodes.png'},
     {label:'Câmbio, Desligo', ano: '2018', img: './assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01459.png'},
     {label:'Cefalópodes', ano: '2018', img: './assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01459.png'},
 
   ]
+  @ViewChild('divVideo') divVideo!: ElementRef;
 
   ngOnInit(): void {
+    if (this.divVideo) {
+      this.divVideo.nativeElement.addEventListener('click', this.playVideo.bind(this));
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.playVideo();
+    this.divVideo.nativeElement.addEventListener('click', this.playVideo.bind(this));
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
     this.playVideo();
   }
 
