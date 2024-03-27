759be349776cc244b3c766a4834ca818b75d1b99
TI-CJC <ti@jardimdacolina.com.br>
2024-03-27 09:04:13 -0300

    att

--- src/app/modules/filme-detalhe/filme-detalhe.component.ts
+++ src/app/modules/filme-detalhe/filme-detalhe.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FilmesService } from 'src/app/shared/services/filmes.service';
 
@@ -9,7 +9,10 @@ import { FilmesService } from 'src/app/shared/services/filmes.service';
 })
 export class FilmeDetalheComponent {
 
+content = 'equipe'
 filme:any
+currentIndex = 0;
+
 constructor(
   private activatedRoute:ActivatedRoute,
   private filmesService: FilmesService
@@ -19,9 +22,17 @@ constructor(
 
 ngOnInit(){
   const id = this.activatedRoute.snapshot.paramMap.get('id');
-  console.log(id);
   this.filme = this.filmesService.listFilms.find(filme => filme.id === id);
-  console.log(this.filme);
+}
 
+@HostListener('window:scroll', ['$event'])
+onScroll(event: any): void {
+  const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+  const newIndex = Math.floor(scrollPosition / (window.innerHeight / 11));
+
+  if (newIndex !== this.currentIndex && newIndex >= 0 && newIndex < this.filme.images_1.length) {
+    this.currentIndex = newIndex;
+  }
 }
 }
--- src/app/shared/services/filmes.service.ts
+++ src/app/shared/services/filmes.service.ts
@@ -8,8 +8,47 @@ export class FilmesService {
   listFilms:any[] = [
     {
       id:'LeComCre',
-      images:[
-        {url:'./assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01459.png'}
+      images_1:[
+        {url:'./assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01459.png'},
+        {url:'./assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01461.png'},
+        {url:'./assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01463.png'},
+        {url:'./assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01465.png'},
+        {url:'./assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01467.png'},
+        {url:'./assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01469.png'},
+        {url:'./assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01471.png'},
+        {url:'./assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01473.png'},
+        {url:'./assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01475.png'},
+        {url:'./assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01477.png'},
+        {url:'./assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01479.png'},
+        {url:'./assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01485.png'}
+      ],
+      images_2:[
+        {url:'./assets/images/01_Lé com Cré/02_LCC_crop/LCC_FINAL_01384.png'},
+        {url:'./assets/images/01_Lé com Cré/02_LCC_crop/LCC_FINAL_01386.png'},
+        {url:'./assets/images/01_Lé com Cré/02_LCC_crop/LCC_FINAL_01388.png'},
+        {url:'./assets/images/01_Lé com Cré/02_LCC_crop/LCC_FINAL_01390.png'},
+        {url:'./assets/images/01_Lé com Cré/02_LCC_crop/LCC_FINAL_01392.png'},
+        {url:'./assets/images/01_Lé com Cré/02_LCC_crop/LCC_FINAL_01394.png'},
+        {url:'./assets/images/01_Lé com Cré/02_LCC_crop/LCC_FINAL_01396.png'},
+        {url:'./assets/images/01_Lé com Cré/02_LCC_crop/LCC_FINAL_01398.png'},
+        {url:'./assets/images/01_Lé com Cré/02_LCC_crop/LCC_FINAL_01402.png'},
+        {url:'./assets/images/01_Lé com Cré/02_LCC_crop/LCC_FINAL_01404.png'},
+        {url:'./assets/images/01_Lé com Cré/02_LCC_crop/LCC_FINAL_01406.png'},
+        {url:'./assets/images/01_Lé com Cré/02_LCC_crop/LCC_FINAL_01408.png'}
+      ],
+      images_3:[
+        {url:'./assets/images/01_Lé com Cré/03_LCC_crop/LCC_FINAL_04414.png'},
+        {url:'./assets/images/01_Lé com Cré/03_LCC_crop/LCC_FINAL_04416.png'},
+        {url:'./assets/images/01_Lé com Cré/03_LCC_crop/LCC_FINAL_04418.png'},
+        {url:'./assets/images/01_Lé com Cré/03_LCC_crop/LCC_FINAL_04420.png'},
+        {url:'./assets/images/01_Lé com Cré/03_LCC_crop/LCC_FINAL_04422.png'},
+        {url:'./assets/images/01_Lé com Cré/03_LCC_crop/LCC_FINAL_04424.png'},
+        {url:'./assets/images/01_Lé com Cré/03_LCC_crop/LCC_FINAL_04425.png'},
+        {url:'./assets/images/01_Lé com Cré/03_LCC_crop/LCC_FINAL_04427.png'},
+        {url:'./assets/images/01_Lé com Cré/03_LCC_crop/LCC_FINAL_04428.png'},
+        {url:'./assets/images/01_Lé com Cré/03_LCC_crop/LCC_FINAL_04430.png'},
+        {url:'./assets/images/01_Lé com Cré/03_LCC_crop/LCC_FINAL_04431.png'},
+        {url:'./assets/images/01_Lé com Cré/03_LCC_crop/LCC_FINAL_04433.png'}
       ],
       title:'LCC_TITLE',
       sub:'LCC_SUBTITLE',
