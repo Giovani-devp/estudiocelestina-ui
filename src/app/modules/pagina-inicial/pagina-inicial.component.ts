810b03ffef9b9aec94c38d39a7d36a191192c6c3
Giovani-devp <giovanips.dev@gmail.com>
2024-04-01 20:35:34 -0300

    .

--- src/app/modules/pagina-inicial/pagina-inicial.component.ts
+++ src/app/modules/pagina-inicial/pagina-inicial.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,49 +6,28 @@ import { Router } from '@angular/router';
   templateUrl: './pagina-inicial.component.html',
   styleUrls: ['./pagina-inicial.component.scss']
 })
-export class PaginaInicialComponent implements OnInit, AfterViewInit{
+export class PaginaInicialComponent implements OnInit {
 
-  listFilmes:any[] = [
-    {id:'LeComCre', label:'Lé com Cré', ano: '2018', img: './assets/images/01_Lé com Cré/LCC.png'},
-    {id:'LeComCre', label:'Cefalópodes', ano: '2018', img: './assets/images/02_Cefalópodes/Cefalópodes.png'},
-    {id:'LeComCre', label:'Câmbio, Desligo', ano: '2018', img: './assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01459.png'},
-    {id:'LeComCre', label:'Cefalópodes', ano: '2018', img: './assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01459.png'},
+  listFilmes: any[] = [
+    { id: 'LeComCre', label: 'Lé com Cré', ano: '2018', img: './assets/images/01_Lé com Cré/LCC.png' },
+    { id: 'LeComCre', label: 'Cefalópodes', ano: '2018', img: './assets/images/02_Cefalópodes/Cefalópodes.png' },
+    { id: 'LeComCre', label: 'Câmbio, Desligo', ano: '2018', img: './assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01459.png' },
+    { id: 'LeComCre', label: 'Cefalópodes', ano: '2018', img: './assets/images/01_Lé com Cré/01_LCC_crop/LCC_FINAL_01459.png' },
 
   ]
   @ViewChild('divVideo') divVideo!: ElementRef;
 
   constructor(
     private route: Router
-  ){
+  ) {
 
   }
 
   ngOnInit(): void {
-    if (this.divVideo) {
-      this.divVideo.nativeElement.addEventListener('click', this.playVideo.bind(this));
-    }
+
   }
 
-  ngAfterViewInit(): void {
-    this.playVideo();
-    this.divVideo.nativeElement.addEventListener('click', this.playVideo.bind(this));
-  }
-
-  @HostListener('window:scroll', ['$event'])
-  onScroll(event: Event) {
-    this.playVideo();
-  }
-
-  playVideo(): void {
-    const video = document.getElementById('video') as HTMLVideoElement;
-    if (video) {
-      video.play().catch(error => {
-        console.error(error);
-      });
-    }
-  }
-
-  navegate(rota:any){
+  navegate(rota: any) {
     this.route.navigate([rota]);
   }
 }
